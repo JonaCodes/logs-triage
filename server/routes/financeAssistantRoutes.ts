8c9071e934078974b0ef2c642061658a0cefe0ba
JonaCodes <jonathanfarache@gmail.com>
2025-10-16 20:16:41 +0300

    add gitignore oops

--- server/routes/financeAssistantRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { askFinanceAssistant } from '../controllers/financeAssistantController';
-
-const router = Router();
-
-router.post('/ask_finance_assistant', askFinanceAssistant);
-
-export default router;
